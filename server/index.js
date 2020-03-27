9fc715dc8ee58076dc28dc7d04b8cbb0014c630a
Ashby Gunter <theshbee@yahoo.com>
2020-03-26 22:35:42 -0700

    Set up pug template and server files to fit team plans for services

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+// const pug = require('pug');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('dev'));
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.use(express.static('client'));
 
 app.get('/:id', (req, res) => {
   const { id } = req.params;
-  console.log('id received is 1');
-  res.send("hey there");
+  console.log(`id received is ${id}`);
+  res.render('index', { productId: id });
 });
 
 app.listen(port, () => {
-  console.log(`Express server for item detail now listening on port ${port}`);
+  console.log(`Express proxy server for etsy item detail now listening on port ${port}`);
 });
